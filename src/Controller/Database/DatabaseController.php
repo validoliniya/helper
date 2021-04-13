63e5442ed01c78d11025cb6366193fa7ae0f5e8f
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2021-04-13 19:01:09 +0300

    security

--- src/Controller/Database/DatabaseController.php
+++ src/Controller/Database/DatabaseController.php
@@ -7,19 +7,21 @@ use App\Form\Database\DatabaseCreateForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/database")
  */
 class DatabaseController extends AbstractController
 {
-    //     * @Security("is_granted('ROLE_ADMIN') or (is_granted('ROLE_USER'))
     /**
      * @Route("/create", name="database.create", methods={"GET", "POST"})
      * @param EntityManagerInterface $entityManager
      * @param Request                $request
+     * @return Response
      */
-    public function create(EntityManagerInterface $entityManager,Request $request){
+    public function create(EntityManagerInterface $entityManager,Request $request): Response
+    {
         $database = new Database();
         $form = $this->createForm(DatabaseCreateForm::class,$database);
         $form->handleRequest($request);
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -32,6 +31,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        return new RedirectResponse('login');
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
